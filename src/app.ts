65592981793d430ef33614eb6bd2674b0e02ae58
Albion Bame <albionbame@gmail.com>
2023-07-01 13:46:05 +0200

    Implement basic authentication
    Implementation of authentication middleware for API endpoints with basic
    authentication.
    
    Username, password and authentication token are configurable via
    environment variables.
    
    Moved HTML templates to root and added build, linter and start scripts.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import dotenv from 'dotenv'
 import express from 'express'
 import nunjucks from 'nunjucks'
 import helmet from 'helmet'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import morgan from 'morgan'
 import routes from './routes'
 
 dotenv.config()
@@ -9,9 +12,19 @@ dotenv.config()
 const port = process.env.PORT ?? 3000
 const app = express()
 
+// adding Helmet to enhance your API's security
 app.use(helmet())
 
-nunjucks.configure('src/views', {
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json())
+
+// enabling CORS for all requests
+app.use(cors())
+
+// adding morgan to log HTTP requests
+app.use(morgan('combined'))
+
+nunjucks.configure('views', {
   autoescape: true,
   express: app
 })
---
+++ src/middleware/security/authentication.ts
@@ -0,0 +1,8 @@
+import type { Request, Response, NextFunction } from 'express'
+export default (request: Request, response: Response, next: NextFunction) => {
+  if (request.headers.authorization === `Basic ${process.env.AUTH_TOKEN}`) {
+    return next()
+  }
+
+  return response.status(403).send({ status: 'fail', message: 'Authorization failed' })
+}
