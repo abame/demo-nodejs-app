b30fecb6fa0923d70e3f468877618c13c613e9cf
Albion Bame <albionbame@gmail.com>
2023-07-01 14:21:13 +0200

    Improved logging
    Improved logging by updating the format and adding request id to server.
    
    Storing logs in a file.

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,27 @@ import helmet from 'helmet'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import morgan from 'morgan'
+import { createWriteStream } from 'fs'
+import { join } from 'path'
+import type { NextFunction, Request, Response } from 'express'
+import { v4 as uuidv4 } from 'uuid'
 import routes from './routes'
 
 dotenv.config()
 
 const port = process.env.PORT ?? 3000
 const app = express()
+
+app.use((request: Request, response: Response, next: NextFunction) => {
+  const requestID = uuidv4()
+  request.headers['X-Request-Id'] = requestID
+  response.set('X-Request-Id', requestID)
+  next()
+})
+
+morgan.token('id', (request: Request) => {
+  return request.headers['X-Request-Id'] as string
+})
 
 // adding Helmet to enhance your API's security
 app.use(helmet())
@@ -22,7 +37,8 @@ app.use(bodyParser.json())
 app.use(cors())
 
 // adding morgan to log HTTP requests
-app.use(morgan('combined'))
+const accessLogStream = createWriteStream(join(__dirname, '../access.log'), { flags: 'a' })
+app.use(morgan(':date[iso] - :id - :remote-addr - :method :url - :response-time ms', { stream: accessLogStream }))
 
 nunjucks.configure('views', {
   autoescape: true,
