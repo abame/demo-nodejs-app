c12cb53a69e7cd0428f648f91a823db38a5367c9
Albion Bame <abame@users.noreply.github.com>
2023-07-01 12:30:11 +0200

    Enabling security via helmet module (#2)
    * Enabling security via helmet module
    
    Implementing security via helmet module by disabling sensitive headers,
    taking care of CORS and XSS, enabling CSP and more.

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import dotenv from 'dotenv'
-
 import express from 'express'
 import nunjucks from 'nunjucks'
+import helmet from 'helmet'
 import routes from './routes'
 
 dotenv.config()
 
 const port = process.env.PORT ?? 3000
 const app = express()
+
+app.use(helmet())
 
 nunjucks.configure('src/views', {
   autoescape: true,
