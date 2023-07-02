6a6ab81677f3d5d01333087d1f909b77032100b8
Albion Bame <albionbame@gmail.com>
2023-07-02 14:41:19 +0200

    Fix error from copy-paste detector linter
    Created JSON file for response of API and test to avoid duplicationa

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,5 +1,6 @@
 /* globals describe, expect, it */
 import request from 'supertest'
+import response from './database/hello-world.json'
 import app from './app'
 
 process.env.AUTH_TOKEN = 'test'
@@ -10,23 +11,6 @@ describe('Hello World API Endpoint', function () {
       .get('/api/v1/hello-world')
       .set('Authorization', 'Basic test')
     expect(res.statusCode).toEqual(200)
-    expect(res.body).toEqual({
-      status: 'success',
-      message: 'Hello World API!',
-      products: [
-        {
-          id: '1',
-          name: 'MacBook Pro'
-        },
-        {
-          id: '2',
-          name: 'HP'
-        },
-        {
-          id: '3',
-          name: 'Dell'
-        }
-      ]
-    })
+    expect(res.body).toEqual(response)
   })
 })
